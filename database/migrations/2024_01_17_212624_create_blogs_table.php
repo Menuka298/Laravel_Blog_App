42924ac73b2e4bc2ea0e9a439761d8e8d9a4ea5a
Menuka Dilshan <67804372+Menuka298@users.noreply.github.com>
2024-01-18 21:39:14 +0530

    Add files via upload

---
+++ database/migrations/2024_01_17_212624_create_blogs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('blog_Tittle');
+            $table->string('blog_description');
+            $table->string('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
